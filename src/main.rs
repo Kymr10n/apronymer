2f1d7eacc1baf03bbeca5e1795ea2107c5bd046c
Kamyrion <kamyrion.lopez@gmail.com>
2022-12-12 18:24:55 +0100

    first steps with permutations, debug still not working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use serde::{Deserialize, Serialize};
+use itertools::Itertools;
 
 struct AppState {
     foo: String,
@@ -24,7 +25,7 @@ fn app_config(config: &mut web::ServiceConfig) {
                 foo: "bar".to_string(),
             }))
             .service(web::resource("/").route(web::get().to(index)))
-            .service(web::resource("/input").route(web::post().to(handle_post_1))),
+            .service(web::resource("/input").route(web::post().to(handle_input))),
     );
 }
 
@@ -36,14 +37,59 @@ async fn index() -> Result<HttpResponse> {
 
 #[derive(Serialize, Deserialize)]
 pub struct MyParams {
-    name: String,
+    tag: String,
+    length: String,
+    count: String,
 }
 
 /// Simple handle POST request
-async fn handle_post_1(params: web::Form<MyParams>) -> Result<HttpResponse> {
+async fn handle_input(params: web::Form<MyParams>) -> Result<HttpResponse> {
+    let mut tags: Vec<&str> = Vec::new();
+
+    tags.push("remote");
+    tags.push("desktop");
+    tags.push("experience");
+    tags.push("secure");
+    tags.push("reliable");
+    tags.push("scalable");
+    tags.push("scalable");
+    tags.push("platform");
+    //tags.push = ["remote", "desktop", "experience", "secure", "reliable", "scalable", "platform"];
+    
+    permutator(tags);
+    
     Ok(HttpResponse::Ok()
         .content_type("text/plain")
-        .body(format!("Your name is {}", params.name)))
+        .body(format!("Your name is {}", params.tag)))
+}
+
+pub struct Fragment {
+    word_index: i32,
+    length: i32,
+}
+
+/// Permutator
+fn permutator(mut tags: Vec<&str>) {
+    let word_index: usize = tags.len();
+
+    //let perms = tags.permutations();
+    let mut permutations: Vec<Vec<&&str>> = Vec::new();
+
+    for permutation in tags.iter().permutations(word_index) {
+        permutations.push(permutation);
+        //permutation.first().unwrap().do_something(vpair.last().unwrap());
+    }
+
+    /*     
+    itertools::assert_equal(perms, vec![
+        vec![5, 6],
+        vec![5, 7],
+        vec![6, 5],
+        vec![6, 7],
+        vec![7, 5],
+        vec![7, 6],
+    ]);
+    */
 }
 
 #[cfg(test)]
@@ -71,6 +117,7 @@ mod tests {
         }
     }
 
+/*
     #[actix_web::test]
     async fn handle_post_1_unit_test() {
         let params = Form(MyParams {
@@ -107,4 +154,5 @@ mod tests {
         let body = to_bytes(resp.into_body()).await.unwrap();
         assert_eq!(body.as_str(), "Your name is John");
     }
+*/
 }
