184b0b26836f6f9889f6f34f5aebee01455f110c
Kymr10n <kamyrion.lopez@gmail.com>
2025-07-24 17:17:16 +0200

    replaced favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="max-w-md mx-auto px-2 sm:px-4 py-4">
       <h1 className="text-2xl font-bold text-left mb-2">
-        Apronymer
+        Apronymers
         {gitVersion && (
           <span className="ml-2 text-xs text-gray-500 align-middle">({gitVersion})</span>
         )}
